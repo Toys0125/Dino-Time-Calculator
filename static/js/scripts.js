0107c01c16c620df6a7198411b97eb9606b07f3d
Toys0125 <marcus.g.stange@gmail.com>
2019-06-18 21:54:32 -0500

    So started in js and the code to power it.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,18 @@
-  $(document).ready(function(){
-    $('.modal').modal();
-  });
+$(document).ready(function () {
+  $('.modal').modal();
+});
+var functions = {
+  TimeCalculate: function (totalMins, maxGrowth, minGrowth, curGrowth, totaltimeLeft) {
+    var remainingTime, seconds;
+    remainingTime = (totalMins / (maxGrowth - minGrowth) * (maxGrowth - curGrowth));
+    seconds = remainingTime % 1;
+    remainingTime -= seconds;
+    seconds = seconds * 60;
+    seconds = Math.floor(Math.pow(10, 1) * seconds) / Math.pow(10, 1);
+    const values = {
+      "remainingTime": remainingTime,
+      "seconds": seconds
+    }
+    return values
+  }
+}
