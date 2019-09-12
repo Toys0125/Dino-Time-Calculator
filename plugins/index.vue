fc63ac91fc9229fd79ef9d68440239ee92529a96
Toys0125 <Marcus.g.stange@gmail.com>
2019-09-12 17:52:14 -0500

    Starting adding saving and then creating objects.

---
+++ plugins/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            dinoData=[],
+            dinoCarnList=["Allo","Cerato","Carno","Dilo","Giga","Sucho","Utah","TRex"],
+            dinoHerbList=["Tricera","Galli","Diablo","Maia","Pachy","Para","Dryo"],
+
+        }
+    },
+    created(){
+        var temp = JSON.parse(localStorage.getItem('Dinodata'))
+        dinoData = temp==null ? [] : temp;
+
+    },
+    methods(){
+        function createDinoObject(){
+            
+        }
+    }
+}
+</script>
