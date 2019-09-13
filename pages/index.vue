9b7eba65055d3dadddc81ebaae825c431383aac3
Toys0125 <Marcus.g.stange@gmail.com>
2019-09-12 21:15:37 -0500

    Moved to pages.

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
-  <v-layout column justify-center align-center>
+    <div>
 
-    <v-flex xs12 sm8 md6>
-      
-    </v-flex>
-  </v-layout>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return{
+            dinoData=[],
+            dinoCarnList=["Allo","Cerato","Carno","Dilo","Giga","Sucho","Utah","TRex"],
+            dinoHerbList=["Tricera","Galli","Diablo","Maia","Pachy","Para","Dryo"],
 
+        }
+    },
+    created(){
+        var temp = JSON.parse(localStorage.getItem('Dinodata'))
+        dinoData = temp==null ? [] : temp;
+
+    },
+    methods(){
+        function createDinoObject(){
+
+            
+        }
+    }
 }
 </script>
